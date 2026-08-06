10056aa7a06f46e52796516cea76beeb2cadcdf8
Malki Roshika <malkiroshika3@gmail.com>
2026-08-06 17:26:17 +0530

    fixes

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -34,7 +34,7 @@ export default async function RootLayout({ children }) {
         {/* {!isProduction() && <meta name="robots" content="noindex, nofollow" />} */}
 
         {settings.data.gtm_code && (
-          <Script id="google-tag-manager" strategy="afterInteractive">
+          <Script id="google-tag-manager" strategy="lazyOnload">
             {`
             (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
